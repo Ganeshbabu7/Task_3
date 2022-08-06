87774fe25e3d194c591001074b73c14192780c67
Ganesh Babu 25 <eganeshbabu7@gmail.com>
2022-08-06 09:42:28 +0530

    Initial commit

---
+++ Doubt.js
@@ -0,0 +1,37 @@
+/*
+For loop in Object
+For loop in Object
+For Each in Object
+
+
+For of Method in Object:
+const student = {
+    Name: "Ravi",
+    Age: 26,
+    School_name: "ABC Matic Hr. Sec School",
+    Address: "Coimbagore"
+}
+console.log("For of Method in Object");
+
+for (const t of student) {
+    console.log(student[t]);
+}
+
+*/
+
+// let request = new XMLHttpRequest
+
+// request.open ("GET","https://docs.google.com/document/d/0B_dOiXd3KMCaU0FqaGY5ZWVqRmc/edit?usp=sharing&ouid=111678760157480811097&resourcekey=0-0h8gWzXUcz3WsqwU3HHF0w&rtpof=true&sd=true")
+
+// request.onload = ()=>{
+//     console.log(request.status);
+//     console.log(request.statuscode);
+//     console.log(request.response);
+// }
+
+// request.error = ()=> {
+//     console.log(request.status);
+//     console.log(request.response);
+// }
+
+// request.send()
